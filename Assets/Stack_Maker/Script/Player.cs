53f43538206e2a8ab849ac6d1f167c80fdacfe8f
Chanhhhh <letrongfls2003@gmail.com>
2023-11-16 21:35:53 +0700

    16/11

---
+++ Assets/Stack_Maker/Script/Player.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+
+public enum Direct { Forward, Back, Left,Right, None}
+public class Player : MonoBehaviour
+{
+    [SerializeField] LayerMask layerBrick;
+    private Vector3 mouseDown, mouseUp;
+    public bool isMove;
+    public Vector3 movePoint;
+    [SerializeField] private float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMove = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMove)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                mouseDown = Input.mousePosition;
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                mouseUp = Input.mousePosition;
+                Direct direct = GetDirect(mouseDown, mouseUp);
+                if(direct != Direct.None)
+                {
+                    movePoint = GetnextPoint(direct);
+                    UnityEngine.Debug.Log(movePoint);
+                    isMove = true;
+                }
+            }
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, movePoint)<0.1f)
+            {
+                isMove = false;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, movePoint, Time.deltaTime*speed);
+        }
+
+    }
+    private Direct GetDirect(Vector3 mouseDown, Vector3 mouseUp)
+    {
+        Direct direct = Direct.None;
+        float delayX = mouseUp.x - mouseDown.x;
+        float delayY = mouseUp.y - mouseDown.y;
+        if(Vector3.Distance(mouseDown, mouseUp) < 100)
+        {
+            direct = Direct.None;
+        }
+        else
+        {
+            if( Mathf.Abs(delayY) > Mathf.Abs(delayX))
+            {
+                if(delayY > 0)
+                {
+                    return Direct.Forward;
+                }
+                else
+                {
+                    return Direct.Back;
+                }
+            }
+            else
+            {
+                if(delayX > 0)
+                {
+                    return Direct.Right;
+                }
+                else
+                {
+                    return Direct.Left;
+                }
+            }
+        }
+        return direct;
+    }
+    private Vector3 GetnextPoint(Direct direct)
+    {
+
+        RaycastHit hit;
+        Vector3 nextPoint = Vector3.zero;
+        Vector3 dir = Vector3.zero;
+
+        switch (direct)
+        {
+            case Direct.Forward:
+                dir = Vector3.forward; break;
+            case Direct.Back:
+                dir = Vector3.back; break;
+            case Direct.Left:
+                dir = Vector3.left; break;
+            case Direct.Right:
+                dir = Vector3.right; break;
+            case Direct.None:
+                break;
+            default:
+                break;
+        }
+
+        for (int i = 0; i < 100; i++){
+            if (Physics.Raycast(transform.position + dir*i,Vector3.down, out hit,10f, layerBrick)) {
+                nextPoint =  hit.collider.transform.position;
+            }
+        }
+
+        return nextPoint;
+    }
+        
+}
