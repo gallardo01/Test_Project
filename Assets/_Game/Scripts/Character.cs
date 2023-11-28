3894cc0e7224442ded29dcaeae9a312fcc87791d
Duong <nguyenthanhduong22072002@gmail.com>
2023-11-28 18:56:56 +0700

    add ui 3

--- Assets/_Game/Scripts/Character.cs
+++ Assets/_Game/Scripts/Character.cs
@@ -7,7 +7,7 @@ public class Character : MonoBehaviour
     [SerializeField] private Transform playerBody;
     [SerializeField] private GameObject BrickPrefab;
 
-    private int totalStack = 0;
+    // private int totalStack = 0;
     private List<GameObject> playerBricks = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
