091f2bc7f7299b38588321278767ea5ff3fc2ea9
Duong <nguyenthanhduong22072002@gmail.com>
2023-11-30 21:33:54 +0700

    move upstair

--- Assets/_Game3/Scripts/PlayerController.cs
+++ Assets/_Game3/Scripts/PlayerController.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speedMove = 5;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask stairLayer;
+    [SerializeField] private Transform center;
+    [SerializeField] private Transform body;
+    [SerializeField] private Animator playerAnim;
+    
+    private string currentAnim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        changeAnim("idle");
     }
 
     // Update is called once per frame
@@ -26,6 +33,57 @@ public class PlayerController : MonoBehaviour
         // if(Input.GetKey(KeyCode.D)){
         //     transform.Translate(Vector3.right * speedMove * Time.deltaTime);
         // }
-        transform.position += JoystickControl.direct * speedMove * Time.deltaTime;
+        
+        if(Input.GetMouseButton(0)){
+            Vector3 nextPositon = transform.position + JoystickControl.direct * speedMove * Time.deltaTime;  
+            transform.position = checkGround(nextPositon);
+
+            changeAnim("run");
+            if(JoystickControl.direct != Vector3.zero){
+                body.forward = JoystickControl.direct;
+            }
+        }
+        if(Input.GetMouseButtonUp(0)){
+            changeAnim("idle");
+        }
+        // Vector3 newPositon = transform.position + JoystickControl.direct * speedMove * Time.deltaTime;  
+        // RaycastHit hit;
+        // if(Physics.Raycast(center.position, newPositon - center.position, out hit, Mathf.Infinity, groundLayer)){
+        //     // playerAnim.SetTrigger("run");
+        //     if(hit.collider.gameObject.transform.position.y <= transform.position.y){
+        //         transform.position = newPositon;
+        //     }
+        //     else{
+        //         transform.position = hit.collider.gameObject.transform.position + new Vector3(0,0.5f,0);
+        //     }
+        // }
+        // else{
+        //     // playerAnim.SetTrigger("idle");
+        // }
+        
+        // else if(Physics.Raycast(center.position, newPositon - center.position, out hit, Mathf.Infinity, stairLayer)){
+        //     // transform.position += new Vector3(0,hit.collider.gameObject.transform.position.y - transform.position.y + 0.5f,0);
+        //     transform.position = hit.collider.gameObject.transform.position + new Vector3(0,0.5f,0);
+        // }
     }
+
+    private Vector3 checkGround(Vector3 point){
+        RaycastHit hit;
+        if(Physics.Raycast(point, Vector3.down, out hit, 2f, stairLayer)){
+            return hit.point + Vector3.up * 1f;
+        }
+        else if(Physics.Raycast(point, Vector3.down, out hit, 2f, groundLayer)){
+            return hit.point - Vector3.up * 1f;
+        }
+        return point;   
+    }
+
+    public void changeAnim(string animName){
+        if(currentAnim != animName){
+            playerAnim.ResetTrigger(currentAnim);
+            currentAnim = animName;
+            playerAnim.SetTrigger(currentAnim);
+        }
+    }
+
 }
--- Assets/_Game3/Scripts/PlayerFoot.cs
+++ Assets/_Game3/Scripts/PlayerFoot.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class PlayerFoot : MonoBehaviour
 {
     [SerializeField] private Transform brickHolder;
-    [SerializeField] private Vector3 nextPos;
+    [SerializeField] private Vector3 nextPosition;
     // Start is called before the first frame update
     void Start()
     {
-        // nextPos = brickHolder.position;
+        // nextPosition = brickHolder.position;
     }
 
     // Update is called once per frame
@@ -20,9 +20,11 @@ public class PlayerFoot : MonoBehaviour
 
     void OnTriggerEnter(Collider other){
         if(other.tag == "Brick"){
-            other.gameObject.transform.parent.transform.SetParent(brickHolder);
-            other.gameObject.transform.parent.transform.position = Vector3.zero;
-            nextPos += new Vector3(0,0.15f,0);
+            other.gameObject.GetComponent<BoxCollider>().enabled = false;
+            other.gameObject.transform.parent.transform.SetParent(brickHolder, false);
+            other.gameObject.transform.parent.transform.localPosition = nextPosition;
+            nextPosition += new Vector3(0,0.16f,0);
+            
         }
     }
 
